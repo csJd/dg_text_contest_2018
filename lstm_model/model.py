fcc64812eed26bb962929a6eed15c96baf364667
pengpeng <552352609@qq.com>
2018-07-26 07:43:58 +0800

    update tf_bdc

--- lstm_model/model.py
+++ lstm_model/model.py
@@ -27,8 +27,7 @@ class Model():
         with tf.name_scope("placeholder"):
 
             #x.shape为 [batch_size,文档单词个数]
-            self.input_x_1 = tf.placeholder(tf.int32,[None,None],name="input_x_1")
-            self.input_x_2 = tf.placeholder(tf.int32,[None,None],name="input_x_2")
+            self.input_x = tf.placeholder(tf.int32,[None,None],name="input_x_1")
             self.input_y = tf.placeholder(tf.float32,[None,num_classes],name="input_y")
 
             self.rnn_input_keep_prob = tf.placeholder(tf.float32,name="rnn_input_keep_prob")
@@ -47,9 +46,6 @@ class Model():
         with tf.variable_scope("rnn_encoder") as share_encoder_scope :
             word_encoded_x1 = self.BidirectionalGRUEncoder(word_embedded_x1,"word_encoder_x1")
 
-        with tf.variable_scope(share_encoder_scope,reuse=tf.AUTO_REUSE):
-            word_encoded_x2 = self.BidirectionalGRUEncoder(word_embedded_x2,"word_encoder_x2")
-
         # 共享attention layer中的所有参数
         # word_encoded.shape [batch , max_doc_lenght , hiddent_size * 2]
         word_context = tf.Variable(tf.truncated_normal(shape=[self.hidden_size * 2], dtype=tf.float32))
@@ -60,10 +56,9 @@ class Model():
                                  name="word_context_bias")
         # word attention
         weight_x1 = self.AttentionLayer(word_encoded_x1,word_context,W_word,b_word,name="attention_1")
-        weight_x2 = self.AttentionLayer(word_encoded_x2,word_context,W_word,b_word,name='attention_2')
 
 #根据新的embedding,和 weights ，计算两个句子的距离。
-        out = self.classifier(word_encoded_x1,weight_x1,word_encoded_x2,weight_x2)
+        out = self.classifier(word_encoded_x1,weight_x1)
 
         self.out = out
 
@@ -125,22 +120,21 @@ class Model():
 
 
     # 将 两个句子表示 之间进行拼接，作为featue_layer,再接一个全连接层，进行分类
-    def classifier(self,word_encoded_x1,weight_x1,word_encoded_x2,weight_x2):
+    def classifier(self,word_encoded_x,weight_x):
 
         #atten_sen1/2.shape 为：[batch_size,hidden_size*2]
+
         # word_encode.shape [batch, max_doc_length, hidden_size*2]
         # weight.shape [batch, max_doc_length , 1]
-        expand_weight_x1 = tf.expand_dims(weight_x1,-1)
-        expand_weight_x2 = tf.expand_dims(weight_x2,-1)
-        atten_sen1 = tf.reduce_sum(tf.multiply(word_encoded_x1,expand_weight_x1),axis=1)
-        atten_sen2 = tf.reduce_sum(tf.multiply(word_encoded_x2,expand_weight_x2),axis=1)
+        expand_weight_x = tf.expand_dims(weight_x,-1)
+        # atten_sens.shape = [batch,]
+        atten_sen = tf.reduce_sum(tf.multiply(word_encoded_x,expand_weight_x),axis=1)
 
         with tf.name_scope("classification"):
 
-            union = tf.concat((atten_sen1,atten_sen2),axis=1)
+            # union = tf.concat((atten_sen1,atten_sen2),axis=1)
 
             # out1 = layers.fully_connected(inputs=union,num_outputs=30,activation_fn=tf.nn.tanh)
-
             out = layers.fully_connected(inputs=union, num_outputs=self.num_classes, activation_fn=None)
 
             predict = tf.argmax(out, axis=1, name="prediction")
