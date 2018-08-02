f72c0f4c27a42d17f271a210f117a490d1b20b1e
Deng <d.vay@qq.com>
2018-08-02 20:23:26 +0800

    Update baseline.py

--- term_weighting_model/baseline.py
+++ term_weighting_model/baseline.py
@@ -2,13 +2,39 @@
 # created by deng on 7/31/2018
 
 import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.svm import LinearSVC
 from sklearn.externals import joblib
 
 import term_weighting_model.tw_to_vector as tw2v
 from utils.path_util import from_project_root
+from term_weighting_model.transformer import TfdcTransformer
 import utils.json_util as ju
+
+
+def tfdc_baseline():
+    column = "word_seg"
+    train_df = pd.read_csv(from_project_root('data/train_set.csv'))
+    test_df = pd.read_csv(from_project_root('data/test_set.csv'))
+    vec = CountVectorizer(ngram_range=(1, 3), min_df=3, max_df=0.8,
+                          max_features=3000000, token_pattern='\w+')
+    trans = TfdcTransformer(sublinear_tf=True)
+    X_train = vec.fit_transform(train_df[column])
+    y_train = (train_df["class"]).astype(int)
+    X_train = trans.fit_transform(X_train, y_train)
+
+    X_test = vec.transform(test_df[column])
+    X_test = trans.transform(X_test)
+
+    lin_clf = LinearSVC()
+    lin_clf.fit(X_train, y_train)
+    preds = lin_clf.predict(X_test)
+
+    result_file = open(from_project_root('processed_data/com_result/baseline_tfdc_3gram_3000000.csv'), 'w')
+    result_file.write("id,class" + "\n")
+    for i, label in enumerate(preds):
+        result_file.write(str(i) + "," + str(label) + "\n")
+    result_file.close()
 
 
 def tfidf_baseline():
